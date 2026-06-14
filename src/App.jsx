3e0bca9a701d9c870520e79ccc735deac6655152
hazelirises <irydiumm@gmail.com>
2026-06-14 16:45:07 -0700

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,60 +2,44 @@ import './App.css'
 import Wave from 'react-wavify'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     
-    <div className = "home">
+    <main className = "home">
+      <h1>hi, i'm hazel!</h1>
       
-      <div>
-        <h1>
-          hi, i'm hazel!
-        </h1>
-        
-      </div>
-
       <div className = "buttons">
-        <button>
-          projects
-        </button>
-        <button>
-          skills
-        </button>
-        <button>
-          about
-        </button>
-        <button>
-          contact
-        </button>
+        <button>projects</button>
+        <button>skills</button>
+        <button>about</button>
+        <button>contact</button>
       </div>
 
       <Wave 
-        className = "bg1"
+        className = "wave bg1"
         fill = "#7C606B"
-        
+        paused = {false}
         options = {{height: 15, amplitude: 30, speed: 0.17, points: 4}}
       />
       <Wave 
-        className = "bg2"
+        className = "wave bg2"
         fill = "#C46BAE"
-        
+        paused = {false}
         options = {{height: 15, amplitude: 30, speed: 0.20, points: 5}}
       />
       <Wave 
-        className = "bg3"
+        className = "wave bg3"
         fill = "#EB7BC0"
-        
+        paused = {false}
         options = {{height: 15, amplitude: 30, speed: 0.23, points: 6}}
       />
       <Wave 
-        className = "bg4"
+        className = "wave bg4"
         fill = "#EDA4BD"
-        
+        paused = {false}
         options = {{height: 15, amplitude: 30, speed: 0.26, points: 8}}
       />
 
-    </div>
+    </main>
   )
 }
 
